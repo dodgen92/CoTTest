9ce57508ceab702d676395f5b91d06c1584ced5b
Tyler R. Dodgen <58264866+dodgen92@users.noreply.github.com>
2023-11-09 18:03:25 -0500

    Update CoTTest.py
    added scroll down and implicit waits

--- CoTTest.py
+++ CoTTest.py
@@ -3,144 +3,115 @@ from selenium.webdriver.chrome.service import Service
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 import time
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import logging
 
 
 
 
 driver = webdriver.Chrome()  # Webdriver-manager will handle path, if not installed specify path of webdriver
 
+logging.info("Navigating to the website")
 driver.get('https://front-stage.cashortrade.org/');     #opens webpage
+logging.info("Opened the website")
 
-time.sleep(10) 
+driver.maximize_window()                          #maximize window
 
-login_btn = driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/header/div[1]/div[2]/div/div[3]/div/button[2]/div/div/p') #xpath to the 'Log in' top right of homepage
-login_btn.click()
 
-time.sleep(5)
+try:
 
-phone_input = driver.find_element(By.ID, 'sign-up-phone-number-tel-input')  #phone number input
-phone_input.send_keys("8888888888")
+    login_btn = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div[1]/header/div[1]/div[2]/div/div[3]/div/button[2]/div/div/p')))
+    login_btn.click()
 
-time.sleep(5)
+    phone_input = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'sign-up-phone-number-tel-input')))
+    phone_input.send_keys("8888888888")
 
-phone_next = driver.find_element(By.ID, 'sign-up-phone-number-next-btn') #phone next button
-phone_next.click()
+    phone_next = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, 'sign-up-phone-number-next-btn')))
+    phone_next.click()
 
-time.sleep(5) 
+    auth2 = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'input-element')))
+    auth2.send_keys('000000')
 
-auth2 = driver.find_element(By.ID, 'input-element')     #inputs 2auth code
-auth2.send_keys('000000')
+    sellbtn = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, 'sell-tickets-header-btn')))
+    sellbtn.click()
 
-time.sleep(5)
+    eventbtn = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="ticket-wallet-wrapper"]/div/div[2]/div[2]/div[1]/button/h3')))
+    eventbtn.click()
+       
+    venuebtn =  WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="ticket-wallet-wrapper"]/div/div[2]/div[2]/button/div/div/div'))) #selects the winery venue
+    venuebtn.click()
+        
+    smtbtn =  WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//div[@id='ticket-wallet-wrapper']/div/div[3]/div[2]/div/button/div[2]"))) #sell my tickets button
+    smtbtn.click()
 
-sellbtn = driver.find_element(By.ID, 'sell-tickets-header-btn')  #sell tickets button top right
-sellbtn.click()
+    xferbtn =  WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div[1]/div[2]/div/div/div[3]/div[2]/div/div/button[1]/div[2]"))) # Transfer ready selection
+    xferbtn.click()
+     
+    xfer_next =  WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "create-listing-next-btn")))                                   # Next button on page 2
+    xfer_next.click()      
 
-time.sleep(5)
+    txquantity =  WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div[1]/div[2]/div/div/div[3]/div[2]/div/div/div[2]/div/div/button/div[1]")))       #ticket quantity selection field
+    txquantity.click()       
 
-eventbtn = driver.find_element(By.XPATH, '//*[@id="ticket-wallet-wrapper"]/div/div[2]/div[2]/div[1]/button/h3')    #navigates to the 'U2 Cover Band', need to randomize if time permits
-eventbtn.click()
+    quantselect =  WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="ticket-wallet-wrapper"]/div/div[3]/div[2]/div/div/div[2]/div/div/div/button[1]')))       # selects ticket quantity from dropdown
+    quantselect.click()    
 
-time.sleep(5)
+    xferrdy =  WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div[1]/div[2]/div/div/div[3]/div[2]/div/div/div[3]/div/button[1]/div")))       
+    xferrdy.click()    
 
-venuebtn = driver.find_element(By.XPATH, '//*[@id="ticket-wallet-wrapper"]/div/div[2]/div[2]/button/div/div/div') #selects the winery venue
-venuebtn.click()
+    ogpurchase =  WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div[1]/div[2]/div/div/div[3]/div[2]/div/div/div[4]/div/div/button/div[1]")))
+    ogpurchase.click()    
 
-time.sleep(5)
+    ogpurchaseselect =  WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div[1]/div[2]/div/div/div[3]/div[2]/div/div/div[4]/div/div/div/button[1]")))
+    ogpurchaseselect.click()    
 
-smtbtn = driver.find_element(By.XPATH, "//div[@id='ticket-wallet-wrapper']/div/div[3]/div[2]/div/button/div[2]") #sell my tickets button
-smtbtn.click()
+    genadmission =  WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div[1]/div[2]/div/div/div[3]/div[2]/div/div/div[5]/div/div/button[1]/div")))
+    genadmission.click()    
 
-time.sleep(5)
+    tixlocation =  WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div[1]/div[2]/div/div/div[3]/div[2]/div/div/div[5]/div/div[2]/div[2]/div/div/div/button")))
+    #tixlocation.click()
+    tixnextbtn = driver.find_element(By.ID, "create-listing-next-btn")                 #finds next button
+    driver.execute_script("arguments[0].scrollIntoView();", tixnextbtn)               #scrolls down until next btn is in view
 
-xferbtn = driver.find_element(By.XPATH, "xpath=//div[@id='ticket-wallet-wrapper']/div/div[3]/div[2]/div/div/button/div[2]")
-xferbtn.click()
+    tixlocation.click()
+    
+    tixlocationselect =  WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div[1]/div[2]/div/div/div[3]/div[2]/div/div/div[5]/div/div[2]/div[2]/div/div/div/div/button[1]")))
+    tixlocationselect.click()
+       
+    #tixnextbtn = driver.find_element(By.ID, "create-listing-next-btn")
+    tixnextbtn.click()      
 
-time.sleep(5)
+    tixprice = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "create-listing-price-per-number-input")))
+    tixprice.send_keys("12")
 
-xfer_next = driver.find_element(By.ID, "create-listing-next-btn")
-xfer_next.click()
+    valueradiobtn = driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/div[2]/div/div/div[3]/div[2]/div/div[3]/label[1]/div/div")
+    valueradiobtn.click()
+    
+    pricenxtbtn =  WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "create-listing-next-btn")))
+    pricenxtbtn.click()
 
-time.sleep(5)
+    time.sleep(2)
 
-txquantity = driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/div[2]/div/div/div[3]/div[2]/div/div/div[2]/div/div/button/div[1]")
-txquantity.click()
+    fundsnxtbtn =  WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "create-listing-next-btn")))
+    fundsnxtbtn.click()
 
-time.sleep(5)
+    time.sleep(2)
 
-quantselect = driver.find_element(By.XPATH, '//*[@id="ticket-wallet-wrapper"]/div/div[3]/div[2]/div/div/div[2]/div/div/div/button[1]')
-quantselect.click()
+    textfield = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div[1]/div[2]/div/div/div[3]/div[1]/div/div[1]/textarea")))
+    textfield.send_keys("TEST")
+          
+    fundsnxtbtn.click()
 
-time.sleep(5)
+    time.sleep(2)
+      
+    fundsnxtbtn.click()
 
-xferrdy = driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/div[2]/div/div/div[3]/div[2]/div/div/div[3]/div/button[1]/div")
-xferrdy.click()
+    print("Test Case passed")
 
-time.sleep(5)
 
-ogpurchase = driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/div[2]/div/div/div[3]/div[2]/div/div/div[4]/div/div/button/div[1]")
-ogpurchase.click()
-
-time.sleep(5)
-
-ogpurchaseselect = driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/div[2]/div/div/div[3]/div[2]/div/div/div[4]/div/div/div/button[1]")
-ogpurchaseselect.click()
-
-time.sleep(5)
-
-genadmission = driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/div[2]/div/div/div[3]/div[2]/div/div/div[5]/div/div/button[1]/div")
-genadmission.click()
-
-time.sleep(5)
-
-tixlocation = driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/div[2]/div/div/div[3]/div[2]/div/div/div[5]/div/div[2]/div[2]/div/div/div/button")
-tixlocation.click()
-
-time.sleep(5)
-
-tixlocationselect = driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/div[2]/div/div/div[3]/div[2]/div/div/div[5]/div/div[2]/div[2]/div/div/div/div/button[1]")
-tixlocationselect.click()
-
-time.sleep(5)
-
-tixnextbtn = driver.find_element(By.ID, "create-listing-next-btn")
-tixnextbtn.click()
-
-time.sleep(5)
-
-tixprice = driver.find_element(By.ID, "create-listing-price-per-number-input")
-tixprice.send_keys("12")
-
-time.sleep(5)
-
-valueradiobtn = driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/div[2]/div/div/div[3]/div[2]/div/div[3]/label[1]/div/div")
-valueradiobtn.click()
-
-time.sleep(5)
-
-pricenxtbtn = driver.find_element(By.ID, "create-listing-next-btn")
-pricenxtbtn.click()
-
-time.sleep(5)
-
-fundsnxtbtn = driver.find_element(By.ID, "create-listing-next-btn")
-fundsnxtbtn.click()
-
-time.sleep(5)
-
-textfield = driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/div[2]/div/div/div[3]/div[1]/div/div[1]/textarea")
-textfield.send_keys("TEST")
-
-time.sleep(5)
-
-fundsnxtbtn.click()
-
-time.sleep(5)
-
-fundsnxtbtn.click()
-
-time.sleep(5)
+finally:
+    driver.quit()
 
 
 
